ed90d6f6812c40dc0171102ac3963b23c0e0f033
Talha Can Havadar <talhahavadar@hotmail.com>
2023-01-18 08:17:20 +0000

    Add no cache header for sdwirec

--- pages/sdwirec.vue
+++ pages/sdwirec.vue
@@ -318,6 +318,10 @@ Dev: 0, Manufacturer: SRPOL, Serial: sdwirec_10, Description: sd-wire</code>
 
 <script>
 export default {
+  middleware({ res }) {
+    console.log(res)
+    res.setHeader('Cache-Control', 'no-cache')
+  },
   async asyncData({ $axios }) {
     const products = (await $axios.get(`/api/stripe/products`)).data
     const product = products.find((p) => p.codename === 'badgerd_sdwirec')
